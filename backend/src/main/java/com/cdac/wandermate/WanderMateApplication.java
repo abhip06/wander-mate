044d18e8fb7c1d39183228d7a84a458b7b77e320
Abhinav Patel <abhipatel1806@gmail.com>
2025-07-22 00:18:41 +0530

    backend init setup

---
+++ backend/src/main/java/com/cdac/wandermate/WanderMateApplication.java
@@ -0,0 +1,13 @@
+package com.cdac.wandermate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanderMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanderMateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cdac/wandermate/WanderMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdac.wandermate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
